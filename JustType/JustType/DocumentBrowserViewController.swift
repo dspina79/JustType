65992baabc56ba6c502be60ca8ba6da966a4055c
Dave Spina <dspina79@gmail.com>
2021-03-23 20:49:49 -0400

    Project initialization and file creator

---
+++ JustType/JustType/DocumentBrowserViewController.swift
@@ -0,0 +1,76 @@
+//
+//  DocumentBrowserViewController.swift
+//  JustType
+//
+//  Created by Dave Spina on 3/23/21.
+//
+
+import UIKit
+
+
+class DocumentBrowserViewController: UIDocumentBrowserViewController, UIDocumentBrowserViewControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        delegate = self
+        
+        allowsDocumentCreation = true
+        allowsPickingMultipleItems = false
+        
+        // Update the style of the UIDocumentBrowserViewController
+        // browserUserInterfaceStyle = .dark
+        // view.tintColor = .white
+        
+        // Specify the allowed content types of your application via the Info.plist.
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    // MARK: UIDocumentBrowserViewControllerDelegate
+    
+    func documentBrowser(_ controller: UIDocumentBrowserViewController, didRequestDocumentCreationWithHandler importHandler: @escaping (URL?, UIDocumentBrowserViewController.ImportMode) -> Void) {
+        let baseDirectory = URL(fileURLWithPath: NSTemporaryDirectory())
+        let fileName = baseDirectory.appendingPathComponent("Untitled.txt")
+        
+        // you must create and save the document then immediately close it and move
+        // it to the importHandler which will handle IOS
+        let document = Document(fileURL: fileName)
+        document.save(to: fileName, for: .forCreating) { success in
+            document.close { success in
+                importHandler(fileName, .move)
+            }
+        }
+    }
+    
+    func documentBrowser(_ controller: UIDocumentBrowserViewController, didPickDocumentsAt documentURLs: [URL]) {
+        guard let sourceURL = documentURLs.first else { return }
+        
+        // Present the Document View Controller for the first document that was picked.
+        // If you support picking multiple items, make sure you handle them all.
+        presentDocument(at: sourceURL)
+    }
+    
+    func documentBrowser(_ controller: UIDocumentBrowserViewController, didImportDocumentAt sourceURL: URL, toDestinationURL destinationURL: URL) {
+        // Present the Document View Controller for the new newly created document
+        presentDocument(at: destinationURL)
+    }
+    
+    func documentBrowser(_ controller: UIDocumentBrowserViewController, failedToImportDocumentAt documentURL: URL, error: Error?) {
+        // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
+    }
+    
+    // MARK: Document Presentation
+    
+    func presentDocument(at documentURL: URL) {
+        
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        let documentViewController = storyBoard.instantiateViewController(withIdentifier: "DocumentViewController") as! DocumentViewController
+        documentViewController.document = Document(fileURL: documentURL)
+        documentViewController.modalPresentationStyle = .fullScreen
+        
+        present(documentViewController, animated: true, completion: nil)
+    }
+}
+
